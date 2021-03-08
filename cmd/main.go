836ef09fe29f786b86fe74c45bd6c2e02ac7d8c3
luca <beccibrian@gmail.com>
2021-03-08 13:00:35 -0300

    chore(@server): Entrypoint added and server working

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/go-kit/kit/log"
+	"github.com/lucabecci/auth-microservices-go/user"
 )
 
 func main() {
@@ -11,7 +13,8 @@ func main() {
 	logger = log.NewLogfmtLogger(os.Stderr)
 	logger = log.With(logger, "listen", "8081", "caller", log.DefaultCaller)
 
-	// svc := user.NewLoggingMiddleware(logger, user.NewService())
-	// r:= user.NewHTTPServer(svc, logger)
-	// logger.Log("msg", "HTTP", )
+	svc := user.NewLoggingMiddleware(logger, user.NewService())
+	r := user.NewHTTPServer(svc, logger)
+	logger.Log("msg", "HTTP", "addr", "8081")
+	logger.Log("err", http.ListenAndServe(":8081", r))
 }
